e785ccda9c2852455d2ab7b29a75dabb4163aaa9
Yvon <yvon.valdepenas@sicpa.com>
2020-01-15 11:41:42 +0100

    Start project

---
+++ StringCalculator/StringCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Valdepenas, Yvon on 15/01/2020.
+//  Copyright © 2020 Sicpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
