69e9cb1eb37285da831e900b548ae2608da7c4c8
Yvon <yvon.valdepenas@sicpa.com>
2020-01-15 12:07:05 +0100

    Fix project for travis

---
+++ StringCalculator/ForTests.swift
@@ -0,0 +1,12 @@
+//
+//  ForTests.swift
+//  StringCalculator
+//
+//  Created by Valdepenas, Yvon on 15/01/2020.
+//  Copyright © 2020 Sicpa. All rights reserved.
+//
+
+import Foundation
+
+
+func sumNumbers(n1: Int = )
